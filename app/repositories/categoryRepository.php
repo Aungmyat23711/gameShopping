d3967dc099533173a908b700b2bc72802d588572
Aung Myint Myat <aungmyat23711@gmail.com>
2022-05-17 15:38:29 +0630

    shop

--- app/repositories/categoryRepository.php
+++ app/repositories/categoryRepository.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\repositories;
 
 use App\interfaces\categoryInterface;
@@ -8,57 +9,62 @@ use App\Models\Totalcategory;
 use Illuminate\Support\Facades\DB;
 use SebastianBergmann\CodeCoverage\Report\Xml\Totals;
 
-class categoryRepository implements categoryInterface{
-    public function add($category){
-        $data=new Category();
-        $data->category=$category->category;
+class categoryRepository implements categoryInterface
+{
+    public function add($category)
+    {
+        $data = new Category();
+        $data->category = $category->category;
         $data->save();
     }
     public function get()
     {
-        $data=Category::all();
+        $data = Category::all();
         return $data;
     }
-    public function del($id){
-        $data=Category::find($id);
+    public function del($id)
+    {
+        $data = Category::find($id);
         $data->delete();
     }
     public function addCategory($category)
     {
-        foreach($category->categories as $cat)
-        {
+        foreach ($category->categories as $cat) {
             // if(Game::find($category->game_id) && Totalcategory::find($cat)){
             // return response()->json(['msg'=>'Already Inserted']);   
             // }else{
-            $data=new Totalcategory();
-            $data->game_id=$category->game_id;
-            $data->category_id=$cat;
+            $data = new Totalcategory();
+            $data->game_id = $category->game_id;
+            $data->category_id = $cat;
             $data->save();
             // }
-           
+
         }
     }
     public function getCategory($id)
     {
-        $data=Totalcategory::where('game_id',$id)->get();
+        $data = Totalcategory::where('game_id', $id)->get();
         return $data;
     }
     public function deletedCategory($id)
     {
-        $data=Totalcategory::find($id);
+        $data = Totalcategory::find($id);
         $data->delete();
     }
-    public function getCategoryById($id){
-       $data=DB::table('totalCategories as TC')->join('categories','TC.category_id','categories.id')->where('TC.game_id',$id)->select('categories.*')->get();
-       return $data;
-    }
-    public function getAllTCount($platform){
-        $data=DB::table('games')->where('platform',$platform)->join('totalcategories as TC','games.id','TC.game_id')
-        ->select('TC.*')->get();
+    public function getCategoryById($id)
+    {
+        $data = DB::table('totalCategories')->join('categories', 'totalCategories.category_id', 'categories.id')->where('totalCategories.game_id', $id)->select('categories.*')->get();
         return $data;
     }
-    public function getAllCountByPlatform($platform){
-        $data=Game::where('platform',$platform)->count();
+    public function getAllTCount($platform)
+    {
+        $data = DB::table('games')->where('platform', $platform)->join('totalcategories', 'games.id', 'totalcategories.game_id')
+            ->select('totalcategories.*')->get();
+        return $data;
+    }
+    public function getAllCountByPlatform($platform)
+    {
+        $data = Game::where('platform', $platform)->count();
         return $data;
     }
 }
